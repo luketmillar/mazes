6cfcdcdfaf340eb1df0964849c37ddf6fa8b329f
Luke Millar <luketmillar@gmail.com>
2020-09-06 22:56:02 -0700

    disable scroll maybe

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,16 @@ import { useLayoutType } from './components/useLayout'
 
 function App() {
   const layoutType = useLayoutType()
+  React.useEffect(() => {
+    const preventBehavior = (e: TouchEvent) => {
+      e.preventDefault()
+    }
+
+    document.addEventListener("touchmove", preventBehavior)
+    return () => {
+      document.removeEventListener("touchmove", preventBehavior)
+    }
+  })
   return (
     <>
       <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
