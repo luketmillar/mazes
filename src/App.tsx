5c91c9c806069652007e20b4dd6be589f84eeaf4
Luke Millar <luketmillar@gmail.com>
2020-09-06 22:47:22 -0700

    more debug

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <>
       <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <div>{window.devicePixelRatio}</div>
         <div>
           <Canvas layoutType={layoutType} />
         </div>
